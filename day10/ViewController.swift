036058d1b9bcd51c03380fb932a5adcec75ed310
Jessica Mallian <jlmallian@gmail.com>
2019-04-26 21:57:52 -0500

    I'm going to bed now.

--- day10/ViewController.swift
+++ day10/ViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+enum APIResult {
+    case success(Data)
+    case failure(Error)
+}
+
 class ViewController: UIViewController {
+    var data: [Book]?
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -17,24 +23,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        callAPI()
+        callAPI(completion: process(apiResult:))
     }
     
-    private func callAPI() {
+    private func callAPI(completion: @escaping (APIResult) -> Void) {
         let url = BookAPI.googleURL(search: "pretty butterfly")
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) { (data, response, error) in
-            if let jsonData = data {
-                if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    print(jsonString)
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    print("unknown error with api call")
                 }
-            } else if let requestError = error {
-                print("Error: \(requestError)")
-            } else {
-                print("Unpected error.")
+                return
             }
+            completion(.success(data))
         }
         task.resume()
+    }
+    
+    private func process(apiResult: APIResult) {
+        switch apiResult {
+        case let .success(apiData):
+            data = createBooks(fromData: apiData)
+        case .failure(_):
+            print("failure with api call")
+        }
+    }
+    
+    private func createBooks(fromData data: Data) -> [Book]? {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+            print(jsonObject)
+        } catch {
+            print(error)
+        }
+        return nil
     }
 }
 
